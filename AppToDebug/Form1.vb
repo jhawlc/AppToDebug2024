10e204d7d2b21d1ece657251c4ac7a9d6bdda962
j.hanke@wlc.ac.uk <j.hanke@wlc.ac.uk>
2023-03-09 16:57:37 +0000

    Add project files.

---
+++ AppToDebug/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim price As Double
+        Dim VATRate As Integer
+
+        VATRate = 20.5
+
+        price = TextBox1.Tex * VATRate
+
+        resultLable.Text = prize
+
+
+End Class
